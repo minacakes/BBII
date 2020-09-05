531d61246a8fced76d33413573ebff64f759026c
nolanpeet <62859034+nolanpeet@users.noreply.github.com>
2020-09-05 17:17:29 -0500

    Thank You and Donation
    Thank You is coded, I just need to script the gif, which shouldn't be too difficult as it's only one element.

---
+++ thankyou.js
@@ -0,0 +1,16 @@
+
+/*     _    ___  _
+      (_)  / __)(_)
+  ____ _ _| |__  _  ___
+ / _  | (_   __)| |/___)
+( (_| | | | | _ | |___ |
+ \___ |_| |_|(_)| (___/
+(_____|       (_*/
+
+var gif = new GIF({
+  workers: 2,
+  quality: 10
+});
+
+// add an image element
+gif.addFrame(imageElement);
